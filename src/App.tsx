7b70928546b9fc76bc7f810283b7bc37235d2629
jackal7819 <jackal3166@gmail.com>
2025-10-01 10:04:02 +0300

    feat: add Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Form from './components/Form'
 import Logo from './components/Logo'
 
 export default function App() {
 	return (
 		<main>
 			<Logo />
+      <Form />
 		</main>
 	);
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,7 @@
+export default function Form() {
+	return (
+		<div>
+			<h2 className='text-3xl font-bold underline'>What do you need for your 😍 trip?</h2>
+		</div>
+	);
+}
