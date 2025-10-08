94479d8ad844e40f0f095cb1c3ec64b77d25db50
jackal7819 <jackal3166@gmail.com>
2025-10-08 20:01:29 +0300

    feat: add checkbox to toggle packed state and strikethrough item name

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,19 +3,23 @@ import type { ItemInterface } from '../App';
 interface ItemProps {
 	item: ItemInterface;
 	onDeleteItem: (id: string) => void;
+	onToggleItem: (id: string) => void;
 }
 
-export default function Item({ item, onDeleteItem }: ItemProps) {
+export default function Item({ item, onDeleteItem, onToggleItem }: ItemProps) {
 	return (
-		<li key={item.id}>
+		<li key={item.id} className='flex items-center gap-5'>
+			<input
+				aria-label='packed'
+				type='checkbox'
+				checked={item.packed}
+				onChange={() => onToggleItem(item.id)}
+				className="transition border-2 rounded appearance-none cursor-pointer size-6 checked:bg-orange-400 checked:border-orange-400 bg-yellow-100 checked:before:content-['✓'] flex items-center justify-center"
+			/>
 			<span className={`capitalize ${item.packed ? 'line-through' : ''}`}>
 				{item.quantity} {item.description}
 			</span>
-			<button
-				type='button'
-				className='ml-5 cursor-pointer'
-				onClick={() => onDeleteItem(item.id)}
-			>
+			<button type='button' className='cursor-pointer' onClick={() => onDeleteItem(item.id)}>
 				❌
 			</button>
 		</li>
--- src/components/PackingList.tsx
+++ src/components/PackingList.tsx
@@ -1,17 +1,23 @@
-import type { ItemInterface } from '../App'
+import type { ItemInterface } from '../App';
 import Item from './Item';
 
 export interface ItemProps {
 	items: ItemInterface[];
 	onDeleteItem: (id: string) => void;
+	onToggleItem: (id: string) => void;
 }
 
-export default function PackingList({ items, onDeleteItem }: ItemProps) {
+export default function PackingList({ items, onDeleteItem, onToggleItem }: ItemProps) {
 	return (
 		<div className='flex-1 w-full p-10 bg-yellow-900'>
 			<ul className='flex justify-center gap-20 mx-auto text-2xl font-semibold text-white max-w-7xl'>
 				{items.map((item) => (
-					<Item key={item.id} item={item} onDeleteItem={onDeleteItem} />
+					<Item
+						key={item.id}
+						item={item}
+						onDeleteItem={onDeleteItem}
+						onToggleItem={onToggleItem}
+					/>
 				))}
 			</ul>
 		</div>
